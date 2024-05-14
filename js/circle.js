989b7d8e24cebc963330cf6bcfd38304d80b85c1
Dohyun Kim <hyun4you2006@naver.com>
2024-05-15 00:29:40 +0900

    drawing speed up

--- js/circle.js
+++ js/circle.js
@@ -1,49 +1,48 @@
 export class Circle {
-    constructor(index, isHypo, radius, speed) {
-        this.index = index;
-        this.isHypo = isHypo;
-        this.radius = radius;
-        this.speed = speed;
-        this.angle = (Math.PI * 3) / 2;
-        this.x = this.prevX = this.x + Math.cos(this.angle) * this.radius;
-        this.y = this.prevY = this.y + Math.sin(this.angle) * this.radius;
-    }
+  constructor(index, isHypo, radius, speed) {
+    this.index = index;
+    this.isHypo = isHypo;
+    this.radius = radius;
+    this.speed = speed;
+    this.angle = (Math.PI * 3) / 2;
+    this.x = this.prevX = this.x + Math.cos(this.angle) * this.radius;
+    this.y = this.prevY = this.y + Math.sin(this.angle) * this.radius;
+  }
 
-    resize(x, y) {
-        this.centerX = x;
-        this.centerY = y;
-    }
+  resize(x, y) {
+    this.centerX = x;
+    this.centerY = y;
+  }
 
-    move() {
-        this.prevX = this.x;
-        this.prevY = this.y;
-        this.x = this.centerX + Math.cos(this.angle) * this.radius;
-        this.y = this.centerY + Math.sin(this.angle) * this.radius;
-        this.cx1 = this.cx2;
-        this.cy1 = this.cy2;
-        this.cx2 = (this.prevX + this.x) / 2;
-        this.cy2 = (this.prevY + this.y) / 2;
+  move() {
+    this.prevX = this.x;
+    this.prevY = this.y;
+    this.x = this.centerX + Math.cos(this.angle) * this.radius;
+    this.y = this.centerY + Math.sin(this.angle) * this.radius;
+    this.cx = (this.prevX + this.x) / 2;
+    this.cy = (this.prevY + this.y) / 2;
 
-        this.update();
-    }
-
-    draw(ctx) {
-        ctx.beginPath();
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = 'rgb(200,255,255)';
-        ctx.shadowColor = 'rgb(0,255,255)';
-        ctx.shadowBlur = 3;
-        ctx.moveTo(this.cx1, this.cy1);
-        ctx.quadraticCurveTo(this.prevX, this.prevY, this.cx2, this.cy2);
-        ctx.stroke();
-
-        /* ctx.beginPath();
-        ctx.fillStyle = 'green';
-        ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);
-        ctx.fill(); */
-    }
-
-    update() {
-        this.angle += this.speed * (this.isHypo && this.index % 2 === 0 ? -1 : 1);
-    }
+    this.update();
+  }
+  /*
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'rgb(200,255,255)';
+    ctx.shadowColor = 'rgb(0,255,255)';
+    ctx.shadowBlur = 3;
+    ctx.moveTo(this.cx1, this.cy1);
+    ctx.quadraticCurveTo(this.prevX, this.prevY, this.cx2, this.cy2);
+    ctx.stroke();
+    
+    // Debug
+    ctx.beginPath();
+    ctx.fillStyle = 'green';
+    ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);
+    ctx.fill(); 
+  }
+  */
+  update() {
+    this.angle += this.speed * (this.isHypo && this.index % 2 === 0 ? -1 : 1);
+  }
 }
--- js/circlegroup.js
+++ js/circlegroup.js
@@ -1,64 +1,104 @@
 import { Circle } from './circle.js';
 
 export class CircleGroup {
-    constructor() {
-        this.zoom = 1;
+  constructor() {
+    this.zoom = 1;
 
-        this.totalCircle = 2;
-        this.speed = 0.05; /* 0.01 <= speed <= 0.1 */
-        this.point = 0;
+    this.totalCircle = 2;
+    this.speed = 0.005; /* 0.001 <= speed <= 0.1 */
+    this.point = 0;
 
-        this.isHypo = false;
-        this.isTrochoid = false;
+    this.isHypo = false;
+    this.isTrochoid = false;
 
-        this.radiuses = [50, 50, 50, 50, 50];
-        this.speeds = [];
-        this.cRadiuses = [];
-        this.circles = [];
+    this.radiuses = [50, 50, 50, 50, 50];
+    this.speeds = [];
+    this.cRadiuses = [];
+    this.circles = [];
 
-        for (let i = 0; i < this.totalCircle; i++) {
-            this.cRadiuses[i] = this.radiuses[i] + (i === this.totalCircle - 1 ? (this.isTrochoid ? this.point : 0) : this.radiuses[i + 1] * (this.isHypo ? -1 : 1));
-        }
+    this.dots = [];
 
-        for (let i = 0; i < this.totalCircle; i++) {
-            this.speeds[i] = i === 0 ? this.speed : (this.radiuses[0] / this.radiuses[i]) * this.speed + this.speeds[i - 1] * (this.isHypo ? -1 : 1);
-        }
-
-        for (let i = 0; i < this.totalCircle; i++) {
-            this.circles[i] = new Circle(i, this.isHypo, this.cRadiuses[i], this.speeds[i]);
-        }
+    for (let i = 0; i < this.totalCircle; i++) {
+      this.cRadiuses[i] =
+        this.radiuses[i] +
+        (i === this.totalCircle - 1
+          ? this.isTrochoid
+            ? this.point
+            : 0
+          : this.radiuses[i + 1] * (this.isHypo ? -1 : 1));
     }
 
-    settings() {
-        this.speeds = [];
-        this.cRadiuses = [];
-        this.circles = [];
-
-        for (let i = 0; i < this.totalCircle; i++) {
-            this.cRadiuses[i] = (this.radiuses[i] + (i === this.totalCircle - 1 ? (this.isTrochoid ? this.point : 0) : this.radiuses[i + 1] * (this.isHypo ? -1 : 1))) * this.zoom;
-        }
-
-        for (let i = 0; i < this.totalCircle; i++) {
-            this.speeds[i] = i === 0 ? this.speed : (this.radiuses[0] / this.radiuses[i]) * this.speed + this.speeds[i - 1] * (this.isHypo ? -1 : 1);
-        }
-
-        for (let i = 0; i < this.totalCircle; i++) {
-            this.circles[i] = new Circle(i, this.isHypo, this.cRadiuses[i], this.speeds[i]);
-        }
+    for (let i = 0; i < this.totalCircle; i++) {
+      this.speeds[i] =
+        i === 0
+          ? this.speed
+          : (this.radiuses[0] / this.radiuses[i]) * this.speed + this.speeds[i - 1] * (this.isHypo ? -1 : 1);
     }
 
-    resize(x, y) {
-        this.circles[0].resize(x, y);
+    for (let i = 0; i < this.totalCircle; i++) {
+      this.circles[i] = new Circle(i, this.isHypo, this.cRadiuses[i], this.speeds[i]);
+    }
+  }
+
+  settings() {
+    this.speeds = [];
+    this.cRadiuses = [];
+    this.circles = [];
+
+    for (let i = 0; i < this.totalCircle; i++) {
+      this.cRadiuses[i] =
+        (this.radiuses[i] +
+          (i === this.totalCircle - 1
+            ? this.isTrochoid
+              ? this.point
+              : 0
+            : this.radiuses[i + 1] * (this.isHypo ? -1 : 1))) *
+        this.zoom;
     }
 
-    draw(ctx) {
-        this.circles[0].move();
-
-        for (let i = 1; i < this.totalCircle; i++) {
-            this.circles[i].resize(this.circles[i - 1].x, this.circles[i - 1].y);
-            this.circles[i].move();
-        }
-
-        this.circles[this.totalCircle - 1].draw(ctx);
+    for (let i = 0; i < this.totalCircle; i++) {
+      this.speeds[i] =
+        i === 0
+          ? this.speed
+          : (this.radiuses[0] / this.radiuses[i]) * this.speed + this.speeds[i - 1] * (this.isHypo ? -1 : 1);
     }
+
+    for (let i = 0; i < this.totalCircle; i++) {
+      this.circles[i] = new Circle(i, this.isHypo, this.cRadiuses[i], this.speeds[i]);
+    }
+  }
+
+  resize(x, y) {
+    this.circles[0].resize(x, y);
+  }
+
+  draw(ctx) {
+    for (let i = 0; i < 99; i++) {
+      this.circles[0].move();
+
+      for (let j = 1; j < this.totalCircle; j++) {
+        this.circles[j].resize(this.circles[j - 1].x, this.circles[j - 1].y);
+        this.circles[j].move();
+      }
+
+      this.dots.push([
+        this.circles[this.totalCircle - 1].x,
+        this.circles[this.totalCircle - 1].y,
+        this.circles[this.totalCircle - 1].cx,
+        this.circles[this.totalCircle - 1].cy,
+      ]);
+    }
+
+    for (let i = 1; i < this.dots.length; i++) {
+      ctx.beginPath();
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = 'rgb(200,255,255)';
+      ctx.shadowColor = 'rgb(0,255,255)';
+      ctx.shadowBlur = 3;
+      ctx.moveTo(this.dots[i - 1][2], this.dots[i - 1][3]);
+      ctx.quadraticCurveTo(this.dots[i - 1][0], this.dots[i - 1][1], this.dots[i][2], this.dots[i][3]);
+      ctx.stroke();
+    }
+    this.dots.splice(0, this.dots.length - 1);
+  }
 }
